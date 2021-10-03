842aa9d3da767dd01b49c4aa5601d98fd5dce1ef
Leonore Yardimli <leonore1982@gmail.com>
2021-10-03 15:43:21 +0800

    Initial Commit

---
+++ SWNavigationSandbox/SWNavigationSandboxApp.swift
@@ -0,0 +1,17 @@
+//
+//  SWNavigationSandboxApp.swift
+//  SWNavigationSandbox
+//
+//  Created by Leonore Yardimli on 2021/10/3.
+//
+
+import SwiftUI
+
+@main
+struct SWNavigationSandboxApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
