418d29079538d32a8f59b7d9f53fa960eebf5860
Leonore Yardimli <leonore1982@gmail.com>
2021-10-06 17:41:43 +0800

    finish wizard part's navigation

--- SWNavigationSandbox/SongChoiceView.swift
+++ SWNavigationSandbox/SongChoiceView.swift
@@ -10,26 +10,31 @@ import SwiftUI
 struct SongChoiceView: View {
 	@State private var isCurrentView = true
 	@EnvironmentObject var wizardElement: WizardElement
+	let screenSize: CGRect = UIScreen.main.bounds
 	
 	var body: some View {
 		if isCurrentView == true {
 			VStack{
-				Text("Song choice")
+				Text("How about this song?")
+					.font(.title2)
+				Text("[\(wizardElement.instrument)] [\(wizardElement.getPrettyCourseTypeName(courseType: wizardElement.courseType))]")
+				Rectangle()
+					.frame(maxWidth: .infinity, maxHeight: .infinity)
+					.overlay(Text("Score viewer + player")
+										.foregroundColor(Color.white))
 				HStack{
 					Button("Sure"){
 						wizardElement.yesNoType = .songChoiceYes
 						withAnimation{
 							self.isCurrentView = false
 						}
-						
-					}
+					}.frame(width: screenSize.width/3)
 					Button("Humm"){
 						wizardElement.yesNoType = .songChoiceNo
 						withAnimation{
 							self.isCurrentView = false
 						}
-						
-					}
+					}.frame(width: screenSize.width/3)
 				}
 			}
 		}else{
